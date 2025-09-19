d5bab188376d787966e0c7a31efd033a153230c8
fadfadhil <fadhilsyawaludin5@gmail.com>
2025-09-19 21:39:09 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,64 @@
+// oprator penjumlahan    
+    const num1 = 5;
+    const num2 = 10;
+    const num3 = 15;    
+    const num4 = 20;
+
+    // oprator penjumlahan fuction
+    const hasilJumlah = num1 + num2;
+    console.log("Hasil dari oprator penjumlahan : " + hasilJumlah);
+
+    const hasilJumlah2 = num2 + num3;
+    console.log("Hasil dari oprator penjumlahan ke 2 : " + hasilJumlah2);
+
+    const hasilJumlah3 = num3 + num4;
+    console.log("hasil dari oprator penjumlahan ke 3 : " + hasilJumlah3);
+
+    console.log("inilah hasil dari oprator penjumlahan 2 angka : "+ hasilJumlah +" , "+ hasilJumlah2 + " , "+  hasilJumlah3);
+    console.log("================OPERATOR PENJUMLAHAN 2 ANGKA==================");
+
+
+
+    //gabugan 3 angka
+    const hasilJumlah4 = num1 + num2 + num3;
+    console.log("hasil dari oprator penjumlahan gabungan 3 angka : " + hasilJumlah4);
+
+    const hasilJumlah5 = num2 + num3 + num4;
+    console.log("hasil dari oprator penjumlahan gabungan 3 angka ke 2 : " + hasilJumlah5);
+
+    console.log("================OPERATOR PENJUMLAHAN 3 ANGKA==================");
+
+
+// oprator penjumlahan selesai   
+
+
+// oprator pengurangan
+    const object1 = 8;
+    const object2 = 16;
+    const object3 = 24;
+    const object4 = 32;
+
+    //operator pengurangan function
+    const hasilKurang = object1 - object2;
+    console.log("hasil dari opratorpenguraangan 1 : "+ hasilKurang);
+
+    const hasilKurang2 = object2 -object1;
+    console.log("hasil dari oprator pengurangan 2 : "+ hasilKurang2);
+
+    const hasilKurang3 = object3 - object2;
+    console.log("hasil dari oprator pengurangan 3 : "+ hasilKurang3);
+
+    console.log("inilah hasil dari oprator pengurangan 2 angka : "+ hasilKurang +" , "+ hasilKurang2 + " , "+  hasilKurang3);
+    console.log("================OPERATOR PENGURANGAN 2 ANGKA==================");
+
+    //gaabungan 3 angka
+    const hasilKurang4 = object4 - object3 - object2;
+    console.log("hasil dari oprator pengurangan gabungan 3 angka yg pertama : " + hasilKurang4);
+
+    const hasilKurang5 = object1 -  object2 -object3;
+    console.log("hasil dari oprator pengurangan gabungan 3 angka yg ke 2 : " + hasilKurang5);
+
+    const hasilKurang6 = object4 - object2 -object3;
+    console.log("hasil dari oprator pengurangan gabugan 3 angka yg ke 3 :" + hasilKurang6)
+
+
